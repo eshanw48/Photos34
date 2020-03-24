3e711a357852ae7b4fc5da36e94e664c4597ce6b
Eshan Wadhwa <eshan.wadhwa@rutgers.edu>
2020-03-24 18:03:08 +0000

    Will finish up UI later today

---
+++ Photos34/src/PhotosView/LoginController.java
@@ -0,0 +1,27 @@
+package PhotosView;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Button Submit;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private Button Clear;
+
+    @FXML
+    private Button Exit;
+
+    @FXML
+    void enterButtonClicked(ActionEvent event) {
+
+    }
+
+}
