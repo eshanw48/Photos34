73fe091a13569d894edfc2cd011209f133d563df
Eshan Wadhwa <eshan.wadhwa@rutgers.edu>
2020-03-24 05:39:34 +0000

    Will finish up UI later today

---
+++ Photos34/AdminController.java
@@ -0,0 +1,30 @@
+package PhotosView;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AdminController {
+
+    @FXML
+    private Button Delete;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private Button Add;
+
+    @FXML
+    private Button Exit;
+
+    @FXML
+    private Button Logout;
+
+    @FXML
+    void enterButtonClicked(ActionEvent event) {
+
+    }
+
+}
