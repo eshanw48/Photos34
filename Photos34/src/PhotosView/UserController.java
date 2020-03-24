b827253374943bdca7292303b17a1a43d25041dd
Eshan Wadhwa <eshan.wadhwa@rutgers.edu>
2020-03-24 18:09:10 +0000

    Will finish UI today

---
+++ Photos34/src/PhotosView/UserController.java
@@ -0,0 +1,44 @@
+package PhotosView;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class UserController {
+
+    @FXML
+    private Button Create;
+
+    @FXML
+    private TextField AlbumName;
+
+    @FXML
+    private Button Exit;
+
+    @FXML
+    private Button Logout;
+
+    @FXML
+    private Button Rename;
+
+    @FXML
+    private Button Delete;
+
+    @FXML
+    private Button Search;
+
+    @FXML
+    private TableView<?> DisplayAlbums;
+
+    @FXML
+    private Button Open;
+
+    @FXML
+    void enterButtonClicked(ActionEvent event) {
+
+    }
+
+}
+
