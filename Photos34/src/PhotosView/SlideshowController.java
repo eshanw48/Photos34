fb48b851fe23f47e8581533a9a3371a1542b9d7d
Eshan Wadhwa <eshan.wadhwa@rutgers.edu>
2020-03-25 04:02:56 +0000

    SlideshowController.java created online with Bitbucket

---
+++ Photos34/src/PhotosView/SlideshowController.java
@@ -0,0 +1,37 @@
+package PhotosView;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class SlideshowController {
+
+    @FXML
+    private ImageView images;
+
+    @FXML
+    private Button exit;
+
+    @FXML
+    private Button previous;
+
+    @FXML
+    private Button next;
+
+    @FXML
+    void exitButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nextButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void previousButton(ActionEvent event) {
+
+    }
+
+}
