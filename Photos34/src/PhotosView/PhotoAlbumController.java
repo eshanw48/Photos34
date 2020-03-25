710039bd1f73fbe975caa86b8b5296b896d850c4
Eshan Wadhwa <eshan.wadhwa@rutgers.edu>
2020-03-25 03:27:56 +0000

    PhotoAlbumController.java created online with Bitbucket

---
+++ Photos34/src/PhotosView/PhotoAlbumController.java
@@ -0,0 +1,97 @@
+package PhotosView;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class PhotoAlbumController {
+
+    @FXML
+    private Button finalize;
+
+    @FXML
+    private TextField caption;
+
+    @FXML
+    private Button restore;
+
+    @FXML
+    private Button exit;
+
+    @FXML
+    private Button logout;
+
+    @FXML
+    private ListView<?> photos;
+
+    @FXML
+    private Button move;
+
+    @FXML
+    private Button copy;
+
+    @FXML
+    private Button display;
+
+    @FXML
+    private Button remove;
+
+    @FXML
+    private Button add;
+
+    @FXML
+    private Button view;
+
+    @FXML
+    void addButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void copyButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void displayButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exitButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void finalizeButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logoutButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void moveButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void removeButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void restoreButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewButton(ActionEvent event) {
+
+    }
+
+}
