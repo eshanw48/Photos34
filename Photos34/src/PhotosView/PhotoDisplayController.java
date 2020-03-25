140d9590c049070fc5f0fb416fa9064ee86e8ebd
Eshan Wadhwa <eshan.wadhwa@rutgers.edu>
2020-03-25 03:56:33 +0000

    PhotoDisplayController.java created online with Bitbucket

---
+++ Photos34/src/PhotosView/PhotoDisplayController.java
@@ -0,0 +1,111 @@
+package PhotosView;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class PhotoDisplayController {
+
+    @FXML
+    private Button finalize;
+
+    @FXML
+    private TextField caption;
+
+    @FXML
+    private Button restore;
+
+    @FXML
+    private Button exit;
+
+    @FXML
+    private Button logout;
+
+    @FXML
+    private ListView<?> photo;
+
+    @FXML
+    private Button album;
+
+    @FXML
+    private Button copy;
+
+    @FXML
+    private Button move;
+
+    @FXML
+    private TextField time;
+
+    @FXML
+    private ListView<?> photo1;
+
+    @FXML
+    private Button key;
+
+    @FXML
+    private Button value;
+
+    @FXML
+    private Button add;
+
+    @FXML
+    private Button remove;
+
+    @FXML
+    void addButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void albumButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void copyButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exitButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void finalizeButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void keyButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logoutButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void moveButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void removeButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void restoreButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void valueButton(ActionEvent event) {
+
+    }
+
+}
