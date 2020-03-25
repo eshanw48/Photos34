e00e432b45b484e815991cfaa2fbf6f026eedb3a
Eshan Wadhwa <eshan.wadhwa@rutgers.edu>
2020-03-25 04:13:51 +0000

    PhotosMoveCopyController.java created online with Bitbucket

---
+++ Photos34/src/PhotosView/PhotosMoveCopyController.java
@@ -0,0 +1,45 @@
+package PhotosView;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class PhotosMoveCopyController {
+
+    @FXML
+    private Button exit;
+
+    @FXML
+    private Button logout;
+
+    @FXML
+    private ListView<?> albums;
+
+    @FXML
+    private Button back;
+
+    @FXML
+    private Button confirm;
+
+    @FXML
+    void backButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void confirmButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exitButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logoutButton(ActionEvent event) {
+
+    }
+
+}
