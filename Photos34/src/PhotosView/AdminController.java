22c0e0a3f9f4c2765b5f80287d61acf2f7f3f194
Eshan Wadhwa <eshan.wadhwa@rutgers.edu>
2020-03-29 04:21:15 +0000

    just did basic buttons

--- Photos34/src/PhotosView/AdminController.java
+++ Photos34/src/PhotosView/AdminController.java
@@ -1,10 +1,21 @@
 package PhotosView;
 
+import java.io.IOException;
+import java.util.Optional;
+
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class AdminController {
 
@@ -28,21 +39,43 @@ public class AdminController {
 
     @FXML
     void addButton(ActionEvent event) {
+    	
+    	
 
     }
 
     @FXML
     void deleteButton(ActionEvent event) {
-
+    	
+    	
     }
 
     @FXML
     void exitButton(ActionEvent event) {
+    	
+    	Platform.exit();
+    	System.exit(0);
 
     }
 
     @FXML
     void logoutButton(ActionEvent event) {
+    	
+    	try {
+			Stage stage = new Stage();
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/PhotosView/Login.fxml"));
+			AnchorPane rootLayout = (AnchorPane) loader.load();
+			
+			Scene scene = new Scene(rootLayout);
+			
+			stage.setScene(scene);
+			((Node)event.getSource()).getScene().getWindow().hide();
+			stage.show();	
+			
+		} catch (IOException m) {
+			m.printStackTrace();
+		}
 
     }
 
