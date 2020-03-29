b6193e5d7210e7c80fab8d3087fb305ad8fda479
Eshan Wadhwa <eshan.wadhwa@rutgers.edu>
2020-03-29 04:29:06 +0000

    just basic buttons

--- Photos34/src/PhotosView/PhotoDisplayController.java
+++ Photos34/src/PhotosView/PhotoDisplayController.java
@@ -1,10 +1,18 @@
 package PhotosView;
 
+import java.io.IOException;
+
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class PhotoDisplayController {
 
@@ -70,6 +78,9 @@ public class PhotoDisplayController {
 
     @FXML
     void exitButton(ActionEvent event) {
+    	
+    	Platform.exit();
+    	System.exit(0);
 
     }
 
@@ -85,6 +96,22 @@ public class PhotoDisplayController {
 
     @FXML
     void logoutButton(ActionEvent event) {
+    	
+    	try {
+			Stage stage = new Stage();
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/PhotosView/Login.fxml"));
+			AnchorPane rootLayout = (AnchorPane) loader.load();
+			
+			Scene scene = new Scene(rootLayout);
+			
+			stage.setScene(scene);
+			((Node)event.getSource()).getScene().getWindow().hide();
+			stage.show();	
+			
+		} catch (IOException m) {
+			m.printStackTrace();
+		}
 
     }
 
