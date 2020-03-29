cc64381f8db4d4188489cbee9b0af737e0acd487
Eshan Wadhwa <eshan.wadhwa@rutgers.edu>
2020-03-29 04:30:41 +0000

    just basic buttons

--- Photos34/src/PhotosView/SlideshowController.java
+++ Photos34/src/PhotosView/SlideshowController.java
@@ -1,9 +1,16 @@
 package PhotosView;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class SlideshowController {
 
@@ -21,6 +28,22 @@ public class SlideshowController {
 
     @FXML
     void exitButton(ActionEvent event) {
+    	
+    	try {
+			Stage stage = new Stage();
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/PhotosView/PhotoAlbum.fxml"));
+			AnchorPane rootLayout = (AnchorPane) loader.load();
+			
+			Scene scene = new Scene(rootLayout);
+			
+			stage.setScene(scene);
+			((Node)event.getSource()).getScene().getWindow().hide();
+			stage.show();	
+			
+		} catch (IOException m) {
+			m.printStackTrace();
+		}
 
     }
 
