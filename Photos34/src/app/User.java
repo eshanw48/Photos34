e1e5e43aaeb018de6d1de186d5153c3c2bf0bb7f
Eshan Wadhwa <eshan.wadhwa@rutgers.edu>
2020-04-01 04:06:28 +0000

    just more classes

--- Photos34/src/app/User.java
+++ Photos34/src/app/User.java
@@ -1,11 +1,19 @@
 package app;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 
 
 public class User implements Serializable{
 	
 	String userName;  
+	
+	private List<Album> albums = new ArrayList<Album>();  // holds all the albums that a User has
+	
+	private List<Photo> userPhotos = new ArrayList<Photo>();
 	
 
 	public User(String userName) { 
@@ -22,7 +30,20 @@ public class User implements Serializable{
 		
 		this.userName = userName;
 	}
-		
 	
+	public void addAlbum(Album album) {
+		
+		albums.add(album);
+	}
+	
+	public void delAlbum(int index) {
+		
+		albums.remove(index);
+	}
+	
+	public Iterator<Album> albumIterator() {
+		
+		return albums.iterator();
+	}
 	
 }
