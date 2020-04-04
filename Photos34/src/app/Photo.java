bd187552c7fe66a4bd0b59222c4da3b832c4a8a0
vishal929 <48373882+vishal929@users.noreply.github.com>
2020-04-04 15:06:12 -0400

    Put compareTo for photos based on date/time. Still need to implement adding tags and stuff.

--- Photos34/src/app/Photo.java
+++ Photos34/src/app/Photo.java
@@ -13,7 +13,7 @@ import java.io.File;
  * @author Eshan Wadhwa and Vishal Patel
  *
  */
-public class Photo implements Serializable {
+public class Photo implements Serializable , Comparable<Photo>{
 	
 	
 	
@@ -56,39 +56,70 @@ public class Photo implements Serializable {
 		
 	}
 
-	
+	public int compareTo(Photo other) {
+		//comparing photos by earlier LocalDateTime
+		return this.getPhotoDate().compareTo(other.getPhotoDate());
+	}
 	/**
-	 * 
-	 * @return
+	 * Getter for photo location on machine.
+	 * @return String that represents the URL Path where the photo is stored on the machine.
 	 */
 	public String getLocation() {
 		return location;
 	}
-
+	
+	/**
+	 * Setter for photo location on machine
+	 * @param location This is the photo path to be set for this photo.
+	 */
 	public void setLocation(String location) {
 		this.location = location;
 	}
 
+	/**
+	 * Getter for the caption of a photo.
+	 * @return Returns a string, which represents the caption of the photo.
+	 */
 	public String getCaption() {
 		return caption;
 	}
-
+	
+	/**
+	 * Setter for the caption of a photo.
+	 * @param caption This is the caption to set for the photo.
+	 */
 	public void setCaption(String caption) {
 		this.caption = caption;
 	}
 
+	/**
+	 * Getter for the date and time of the photo.
+	 * @return Returns the LocalDateTime instance that represents the date and time of the photo, based on the machines timezone.
+	 */
 	public LocalDateTime getPhotoDate() {
 		return photoDate;
 	}
 
+	/**
+	 * Setter for the date and time of the photo.
+	 * @param photoDate The localDateTime instance to be set for the Photo.
+	 */
 	public void setPhotoDate(LocalDateTime photoDate) {
 		this.photoDate = photoDate;
 	}
 
+	/**
+	 * Getter for the list of tags of a photo.
+	 * @return Returns a list, which represents the list of tags associated with the photo.
+	 */
 	public List<Tag> getPhotoTags() {
 		return photoTags;
 	}
 
+	/**
+	 * Setter for the list of tags for the photo.
+	 * @param photoTags This is the list of tags to be set for the photo.
+	 */
 	public void setPhotoTags(List<Tag> photoTags) {
 		this.photoTags = photoTags;
 	}
