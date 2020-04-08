26dcaa534f44e8178c0ba703cbeacdc69ac442ab
Eshan Wadhwa <eshan.wadhwa@rutgers.edu>
2020-04-08 00:08:12 +0000

    saving and reading should work now

--- Photos34/src/app/Photo.java
+++ Photos34/src/app/Photo.java
@@ -2,10 +2,14 @@ package app;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
 import java.time.LocalDateTime;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.util.List;
+
+
 import java.io.File;
 
 /**
@@ -13,115 +17,191 @@ import java.io.File;
  * @author Eshan Wadhwa and Vishal Patel
  *
  */
-public class Photo implements Serializable , Comparable<Photo>{
+public class Photo implements Serializable{
 	
 	
 	
-	/**
-	 * Stores the address of the photo on the user's machine. Intention is to then refer to the photo and display it via URL.
-	 */
+	private String photoName;
+	
 	private String location;
 	
-	/**
-	 * User made caption for the photo.
-	 */
-	private String caption;
+	String caption = "";
+				
+	private Date photoDate;
 	
-	/**
-	 * Date for the "last modified" attribute of the photo file on the user's system in ISO-8601. Has date and time which considers the user's timezone.
-	 */
-	private LocalDateTime photoDate;
+	private List<Tag> photoTags = new ArrayList<Tag>();
 	
-	/**
-	 * Each photo has a list of tags that the user can refer to in order to find this photo.
-	 */
-	private List<Tag> photoTags;
-	
-	/**
-	 * Constructor to create an instance that stores our info for a specific photo. 
-	 * @param caption This is the string that the user wants to caption the photo with.
-	 * @param found This is the photo file that is passed by our java controller, which checks if this file exists before passing it here. So, this file is guaranteed to exist and it is a photo file.
-	 */
-	public Photo(String caption, File found)
+	public Photo(String name, String location)
 	{
-		this.caption = caption;
-		this.photoTags=new ArrayList<Tag>();
-		
-		//setting the date of the file
-		long milliseconds = found.lastModified();
-		Instant mill = Instant.ofEpochMilli(milliseconds);
-		
-		//LocalDateTime instance for our photos
-		this.photoDate= LocalDateTime.ofInstant(mill,ZoneId.systemDefault());
-		
+		photoName = name;
+		this.location = location;
 	}
+	
+	public boolean addTag(String name, String value)
+	{
+		for (Iterator<Tag> i = photoTags.iterator(); i.hasNext();)
+		{
+			Tag element = i.next();
+			if(element.isEqual(name, value))
+			{
+				return false;
+			}
+		}
+		photoTags.add(new Tag(name, value));
+		
+		return true;
+	}
+	
+	public boolean removeTag(String name, String value)
+	{
+		for (Iterator<Tag> i = photoTags.iterator(); i.hasNext();)
+		{
+			Tag element = i.next();
+			if(element.isEqual(name, value))
+			{
+				i.remove();
+				return true;
+			}
+		}
 
-	public int compareTo(Photo other) {
-		//comparing photos by earlier LocalDateTime
-		return this.getPhotoDate().compareTo(other.getPhotoDate());
+		return false;
 	}
-	/**
-	 * Getter for photo location on machine.
-	 * @return String that represents the URL Path where the photo is stored on the machine.
-	 */
-	public String getLocation() {
+	
+	public Iterator<Tag> tagIterator() {
+		
+		return photoTags.iterator();
+	}
+	
+	public String getPhotoName()
+	{
+		return photoName;
+	}
+	
+	public void setPhotoName(String name)
+	{
+		photoName = name;
+	}
+	
+	public String getPhotoLocation()
+	{
 		return location;
 	}
 	
-	/**
-	 * Setter for photo location on machine
-	 * @param location This is the photo path to be set for this photo.
-	 */
-	public void setLocation(String location) {
-		this.location = location;
+	public void setPhotoLocation(String filePath)
+	{
+		location = filePath;
 	}
-
-	/**
-	 * Getter for the caption of a photo.
-	 * @return Returns a string, which represents the caption of the photo.
-	 */
-	public String getCaption() {
+	
+	public String getCaption()
+	{
 		return caption;
 	}
 	
-	/**
-	 * Setter for the caption of a photo.
-	 * @param caption This is the caption to set for the photo.
-	 */
-	public void setCaption(String caption) {
+	public void setCaption(String caption)
+	{
 		this.caption = caption;
 	}
-
-	/**
-	 * Getter for the date and time of the photo.
-	 * @return Returns the LocalDateTime instance that represents the date and time of the photo, based on the machines timezone.
-	 */
-	public LocalDateTime getPhotoDate() {
+	
+	public Date getPhotoDate() {
+		
 		return photoDate;
 	}
-
-	/**
-	 * Setter for the date and time of the photo.
-	 * @param photoDate The localDateTime instance to be set for the Photo.
-	 */
-	public void setPhotoDate(LocalDateTime photoDate) {
-		this.photoDate = photoDate;
+	
+	public String getPhotoDateString()
+	{
+		return photoDate.toString();
+	}
+	
+	public void setPhotoDate(Date d) {
+		
+		photoDate = d;
 	}
 
+	public String getTags(String name)
+	{
+		StringBuilder tagList = new StringBuilder();
+		
+		for (Iterator<Tag> i = photoTags.iterator(); i.hasNext();)
+		{
+			Tag element = i.next();
+			String tagSet[] = element.toString().split("~");
+			if (tagSet[0].equals(name))
+			{
+				tagList.append(tagSet[1] + ",");
+			}
+		}
+		
+		if (tagList.length() > 0)
+		{
+			tagList.deleteCharAt(tagList.length()-1);
+		}
+		
+		return tagList.toString();
+	}
+	
+	public void setTags(String name, String tags)
+	{
+		String[] tagArray = tags.split(",");
+		
+		for (int i = 0; i < tagArray.length; i++)
+		{
+			Tag tag = new Tag(name, tagArray[i]);
+			photoTags.add(tag);
+		}
+	}
+	
+	public String getAllTags()
+	{
+		StringBuilder tagList = new StringBuilder();
+		
+		for (Iterator<Tag> i = photoTags.iterator(); i.hasNext();)
+		{
+			Tag element = i.next();
+			String tagSet[] = element.toString().split("~");
+			tagList.append(tagSet[1] + ",");
+		}
+		
+		if (tagList.length() > 0)
+		{
+			tagList.deleteCharAt(tagList.length()-1);
+		}
+		
+		return tagList.toString();
+	}
+	
 	/**
-	 * Getter for the list of tags of a photo.
-	 * @return Returns a list, which represents the list of tags associated with the photo.
+	 * 
+	 * @param p
+	 * @return true if two photos share the same file location
 	 */
-	public List<Tag> getPhotoTags() {
-		return photoTags;
+	public boolean isFileLocationEqual(Photo p)
+	{
+		return this.getPhotoLocation().compareTo(p.getPhotoLocation()) == 0;
+	}
+	
+	/**
+	 * 
+	 * @param p
+	 * @return true if two photos are equal
+	 */
+	public boolean isEqual(Photo p)
+	{
+		return this.getPhotoName().compareTo(p.getPhotoName()) == 0
+				&& this.getPhotoLocation().compareTo(p.getPhotoLocation()) == 0
+				&& this.getPhotoDateString().compareTo(p.getPhotoDateString()) == 0
+				&& this.getCaption().compareTo(p.getCaption()) == 0
+				&& this.getAllTags().compareTo(p.getAllTags()) == 0;
+	}
+	
+	/**
+	 * returns the photoName
+	 */
+	public String toString()
+	{
+		return getPhotoName();
 	}
 
-	/**
-	 * Setter for the list of tags for the photo.
-	 * @param photoTags This is the list of tags to be set for the photo.
-	 */
-	public void setPhotoTags(List<Tag> photoTags) {
-		this.photoTags = photoTags;
-	}
+
 
 }
+
