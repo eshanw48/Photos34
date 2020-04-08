8513ca04a57334d55eb913a435ee9f16d425e001
Eshan Wadhwa <eshan.wadhwa@rutgers.edu>
2020-04-08 00:11:23 +0000

    added readUser method here

--- Photos34/src/app/Photos.java
+++ Photos34/src/app/Photos.java
@@ -4,16 +4,46 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
 
 public class Photos extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+    	
+
+		try {
+			
+			Persistance.readUser();
+			
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/PhotosView/Login.fxml"));
+			AnchorPane root = (AnchorPane) loader.load();
+			
+			
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+		/*
+    	Persistance.readUser();
+    	
         Parent root = FXMLLoader.load(getClass().getResource("/PhotosView/Login.fxml"));
         primaryStage.setTitle("Photos By: Vishal Patel and Eshan Wadhwa");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        
+        
+       */
     }
 
 
@@ -21,6 +51,3 @@ public class Photos extends Application {
         launch(args);
     }
 }
-
-
-
